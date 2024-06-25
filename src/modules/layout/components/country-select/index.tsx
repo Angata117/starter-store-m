47bd19e2ec546230afae85a119b52668f88f2a62
Angel Kolarov <angelkolarov17a@gmail.com>
2024-06-25 15:52:46 +0300

    Update index.tsx

--- src/modules/layout/components/country-select/index.tsx
+++ src/modules/layout/components/country-select/index.tsx
@@ -57,7 +57,7 @@ const CountrySelect = ({ toggleState, regions }: CountrySelectProps) => {
     <div>
       <Listbox
         as="span"
-        onChange={(value : CountryOption)=>handleChange(value as CountryOption)}
+        onChange={(value)=>handleChange(value as CountryOption)}
         defaultValue={
           countryCode
             ? options?.find((o) => o.country === countryCode)
